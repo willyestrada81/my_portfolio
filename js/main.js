ffcc0d2f61ca004c20f6c9e0fca7b6399f647277
williamestrada <willy.estrada81@gmail.com>
2019-09-13 12:13:42 -0400

    Updates

---
+++ js/main.js
@@ -0,0 +1,60 @@
+// Select DOM items
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuBranding = document.querySelector('.menu-branding');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.nav-item');
+
+//Set initial state of menu
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+navItems.forEach(item => item.addEventListener('click', () => {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuBranding.classList.remove('show');
+    menuNav.classList.remove('show');
+    navItems.forEach(item => item.classList.remove('show'));
+
+    showMenu = false;
+}));
+
+
+function toggleMenu() {
+    if(!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuBranding.classList.add('show');
+        menuNav.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+        
+        showMenu = true;
+
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuBranding.classList.remove('show');
+        menuNav.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        showMenu = false;
+    }
+}
+
+window.onscroll = () => {
+    const nav = document.querySelector("#navbar");
+    const logo = document.querySelector(".my-logo");
+    if (this.scrollY <= 10) {
+        nav.className = "";
+        logo.src = "/dist/img/william Estrada logo.PNG";
+    } else {
+        nav.className = "scroll";
+        logo.src = "/dist/img/william Estrada logo_black.PNG";
+
+    }
+};
+
+
